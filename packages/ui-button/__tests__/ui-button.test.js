07c932bbac556fd793b0b52932b8dd40f97b8fa5
xiukun@163.com <xiukun@163.com>
2023-04-17 16:36:55 +0800

    fix: clean

---
+++ packages/ui-button/__tests__/ui-button.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const uiButton = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(uiButton(), 'Hello from uiButton');
+console.info('uiButton tests passed');
